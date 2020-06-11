1034f207bd66aa5c3a9639590a224382db19d854
Patryk Gwiazdowski <pgwiazdowski@gmail.com>
2020-06-12 01:07:07 +0200

    Added sample scene. Added simple enemy AI which makes them follow the path.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+
+    private Transform target;
+    private int waypointIndex;
+
+    void Start()
+    {
+        SetNewTarget();
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            SetNewTarget();
+        }
+    }
+
+    private void SetNewTarget()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        target = Waypoints.waypoints[waypointIndex];
+        waypointIndex++;
+    }
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float waveInternalSec = 10f;
+    [SerializeField] private Transform spawnPoint;
+    private float countdown = 2;
+    private int waveNumber = 1;
+    private float enemySpawnInterval = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f)
+        {
+            countdown = waveInternalSec;
+            StartCoroutine(SpawnWave());
+        }
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < GetMonstersInCurrentWave(); i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(enemySpawnInterval);
+        }
+        waveNumber++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    private int GetMonstersInCurrentWave()
+    {
+        return waveNumber;
+    }
+}
